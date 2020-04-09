2e2afd08d0bffd12b1e9e801ec80d7eb5ebbbc7f
nekisse <lsh891224@gmail.com>
2020-04-09 13:35:00 +0900

    pro27  tiles 라이브러리 사용

--- pro27/src/main/java/com/myspring/pro27/member/dao/MemberDAO.java
+++ pro27/src/main/java/com/myspring/pro27/member/dao/MemberDAO.java
@@ -7,9 +7,12 @@ import org.springframework.dao.DataAccessException;
 import com.myspring.pro27.member.vo.MemberVO;
 
 public interface MemberDAO {
-	 public List selectAllMemberList() throws DataAccessException;
-	 public int insertMember(MemberVO memberVO) throws DataAccessException ;
-	 public int deleteMember(String id) throws DataAccessException;
-	 public MemberVO loginById(MemberVO memberVO) throws DataAccessException;
+    public List selectAllMemberList() throws DataAccessException;
+
+    public int insertMember(MemberVO memberVO) throws DataAccessException;
+
+    public int deleteMember(String id) throws DataAccessException;
+
+    public MemberVO loginById(MemberVO memberVO) throws DataAccessException;
 
 }
